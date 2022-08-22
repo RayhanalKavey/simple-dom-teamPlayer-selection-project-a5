b37d675443e5b9c53a0612a45d74d82b7855aa37
RayhanalKavey <rayhanalkavey@gmail.com>
2022-08-22 07:23:34 +0600

    complete budget calculation.

--- js/budget.js
+++ js/budget.js
@@ -1,21 +1,32 @@
-function getInputValueByID(ID) {
-  const inputField = document.getElementById(ID);
-  return inputField.value;
-}
 //Calculate player expense button
 document
-  .getElementById("player-expenses")
+  .getElementById("player-expenses-btn")
   .addEventListener("click", function () {
     const perPlayerCost = getInputValueByID("per-player-cost");
-    console.log(perPlayerCost);
-    // console.log("player expense button clicked.");
+    ///Validation: 1) Number must be positive. 2) Must provide a number.
+    if (perPlayerCost < 0) {
+      return alert("Please provide positive amount.");
+    } else if (isNaN(perPlayerCost)) {
+      return alert("Please provide valid amount.");
+    }
+    const playerExpenses = perPlayerCost * 5;
+    setElementValueByID("player-expenses", playerExpenses);
+    document.getElementById("total-expenses-btn").removeAttribute("disabled");
   });
 //Calculate total expense button
 document
-  .getElementById("total-expenses")
+  .getElementById("total-expenses-btn")
   .addEventListener("click", function () {
+    const playerExpenses = getElementValueByID("player-expenses");
     const managerCost = getInputValueByID("manager-cost");
     const coachCost = getInputValueByID("coach-cost");
-    console.log(managerCost, coachCost);
-    // console.log("Total expense button clicked.");
+    ///Validation: 1) Number must be positive. 2) Must provide a number.
+    if (managerCost < 0 || coachCost < 0) {
+      return alert("Please provide positive amount.");
+    } else if (isNaN(managerCost) || isNaN(coachCost)) {
+      return alert("Please provide valid amount.");
+    }
+
+    const totalCost = playerExpenses + managerCost + coachCost;
+    setElementValueByID("total-expenses", totalCost);
   });
--- js/utility.js
+++ js/utility.js
@@ -1,1 +1,15 @@
-
+//Get input field value by ID
+function getInputValueByID(ID) {
+  const inputValue = parseFloat(document.getElementById(ID).value);
+  return inputValue;
+}
+//Get element  value by ID
+function getElementValueByID(ID) {
+  const elementValue = parseFloat(document.getElementById(ID).innerText);
+  return elementValue;
+}
+//Set element  value by ID
+function setElementValueByID(ID, value) {
+  const elementField = document.getElementById(ID);
+  elementField.innerText = value;
+}
