740e6d7fe203b196a093a96c76d75957d9327eb6
RayhanalKavey <rayhanalkavey@gmail.com>
2022-08-22 09:46:01 +0600

    add onclick attribute to all the html button of the card, and add addToSelectionBox function from where, got the array of names. Also add counts to the selection box and check whether the five times selection done.

--- js/select5.js
+++ js/select5.js
@@ -1,3 +1,16 @@
+const cardArray = [];
+
 function addToSelectionBox(element) {
-  console.log("button clicked");
+  const playerName = element.parentNode.parentNode.children[0].innerText;
+
+  cardArray.push(playerName);
+  //Validation: Set updated count and check if the count is grater then 5
+  const countPlayers = cardArray.length;
+  if (countPlayers <= 5) {
+    setElementValueByID("total-selection", countPlayers);
+  } else {
+    return alert("Already 5 players added.");
+  }
+  //call for display function
+  displaySelectedPlayer(cardArray);
 }
