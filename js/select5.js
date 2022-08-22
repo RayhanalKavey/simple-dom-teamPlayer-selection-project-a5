fc53211d43cf6b6d63d5ec03afb9034a18c4e780
RayhanalKavey <rayhanalkavey@gmail.com>
2022-08-22 10:21:25 +0600

    add function displaySelectedPlayer to display the selected players name in the display area. Do validation for one button cannot be clicked multiple times.

--- js/select5.js
+++ js/select5.js
@@ -1,8 +1,19 @@
 const cardArray = [];
-
+function displaySelectedPlayer(names) {
+  const orderedListField = document.getElementById("lists-body");
+  orderedListField.innerText = "";
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    const li = document.createElement("li");
+    li.className = "mb-4";
+    li.innerText = name;
+    orderedListField.appendChild(li);
+  }
+}
 function addToSelectionBox(element) {
   const playerName = element.parentNode.parentNode.children[0].innerText;
-
+  //disable the clicked button
+  element.setAttribute("disabled", "");
   cardArray.push(playerName);
   //Validation: Set updated count and check if the count is grater then 5
   const countPlayers = cardArray.length;
@@ -14,3 +25,6 @@ function addToSelectionBox(element) {
   //call for display function
   displaySelectedPlayer(cardArray);
 }
+// const c = document.getElementById("player-expenses").parentNode;
+// console.log(c);
+//  document.getElementById("total-expenses-btn").removeAttribute("disabled");
